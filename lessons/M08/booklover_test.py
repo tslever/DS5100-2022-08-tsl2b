84a421f5a7eb20d5846ede2e8b82f7a2ae59fec9
tslever <46533664+tslever@users.noreply.github.com>
2022-10-19 04:14:58 -0400

    Downloaded prompt for homework on Python testing. Created the before-code portion of hw08.ipynb. Drafted booklover.py. Tested BookLover through adding one book in each of two separate test methods.
    Going to have the test_2_add_book attempt to add a second book, fail if an exception is not thrown, and succeed if an exception in caught

---
+++ lessons/M08/booklover_test.py
@@ -0,0 +1,140 @@
+'''
+Module for class BookLoverTestSuite, which tests the methods of a BookLover
+'''
+
+import unittest
+from booklover import *
+
+class BookLoverTestSuite(unittest.TestCase):
+    '''
+    Tests the methods of a BookLover
+
+    Instance variables:
+        none
+
+    Public methods:
+        test_1_init
+    '''
+
+    def setUp(self):
+        '''
+        Creates a instance variable with a value of a BookLover
+
+        Keyword arguments:
+            none
+
+        Return values:
+            none
+
+        Side effects:
+            Creates an instance variable with a value of a BookLover
+
+        Exceptions raised:
+            none
+
+        Restrictions on when this method can be called:
+            none
+        '''
+
+        self.book_lover = BookLover('Han Solo', 'hsolo@millenniumfalcon.com', 'scifi')
+        #self.book_lover = BookLover('Han Solo', 'hsolo@millenniumfalcon.com', 'scifi', book_list = pd.DataFrame({'book_name':['S'], 'book_rating':[1]}))
+
+    def test_0_init(self):
+        '''
+        Tests BookLover.__init__
+
+        Keyword arguments:
+            none
+
+        Return values:
+            none
+
+        Side effects:
+            Tests whether a BookLover's name, email, favorite genre, number of read books, and book list are equal to expected values
+
+        Exceptions raised:
+            AssertionError if a BookLover's name, email, favorite genre, number of read books, or book list is not equal to an expected value
+
+        Restrictions on when this method can be called:
+            none
+        '''
+
+        self.assertEqual(self.book_lover.name, 'Han Solo')
+        self.assertEqual(self.book_lover.email, 'hsolo@millenniumfalcon.com')
+        self.assertEqual(self.book_lover.fav_genre, 'scifi')
+        self.assertEqual(self.book_lover.num_books, 0)
+        self.assertTrue(self.book_lover.book_list.equals(pd.DataFrame({'book_name':[], 'book_rating':[]})))
+
+
+    def test_1_add_book(self):
+        '''
+        Tests BookLover.add_book by confirming that a book was successfully added to a BookLover's read book list
+
+        Keyword arguments:
+            none
+
+        Return values:
+            none
+
+        Side effects:
+            Adds a book to the read-book list of a BookLover and tests whether the book is in the BookLover's read-book list
+
+        Exceptions raised:
+            AssertionError if a book, including book name and rating, is not correctly added to the read-book list of a BookLover
+
+        Restrictions on when this method can be called:
+            none
+        '''
+        
+        self.book_lover.add_book('Star Wars: A New Hope', 5)
+        self.assertTrue(self.book_lover.book_list.equals(pd.DataFrame({'book_name':['Star Wars: A New Hope'], 'book_rating':[5]})))
+        # Don't use assert True
+
+    def test_2_add_book(self):
+        '''
+        Tests BookLover.add_book by running test_1_add_book twice
+
+        Keyword arguments:
+            none
+
+        Return values:
+            none
+
+        Side effects:
+            Adds a book to the read-book list of a BookLover and tests whether the book is in the BookLover's read-book list
+
+        Exceptions raised:
+            AssertionError if a book, including book name and rating, is not correctly added to the read-book list of a BookLover
+
+        Restrictions on when this method can be called:
+            none
+        '''
+        
+        self.book_lover.add_book('Star Wars: A New Hope', 5)
+        self.assertTrue(self.book_lover.book_list.equals(pd.DataFrame({'book_name':['Star Wars: A New Hope'], 'book_rating':[5]})))
+
+    def tearDown(self):
+        '''
+        Deletes an instance variable with a value of a BookLover
+
+        Keyword arguments:
+            none
+
+        Return values:
+            none
+
+        Side effects:
+            Deletes an instance variable with a value of a BookLover
+
+        Exceptions raised:
+            none
+
+        Restrictions on when this method can be called:
+            none
+        '''
+
+        del self.book_lover
+
+if __name__ == '__main__':
+    verbose = 3
+    unittest.main(verbosity = verbose)
